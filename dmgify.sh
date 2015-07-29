4f278bbaa806306239527339d7467415ce6184eb
ian <barkatthetree@gmail.com>
2015-07-29 12:13:08 -0700

    added default param

--- dmgify.sh
+++ dmgify.sh
@@ -1,130 +1,111 @@
 #! /bin/bash
 
-# Create a read-only disk image of the contents of a folder
-
 set -e;
 
-
-
-function todo() {
-  echo "TODO"
-}
-
-function usage() {
-  version
-  echo "Creates a fancy DMG file."
-  echo "Usage:  $(basename $0) options... image.dmg source_folder"
-  echo "All contents of source_folder will be copied into the disk image."
-  echo "Options:"
-  echo "  --volname name"
-  echo "      set volume name (displayed in the Finder sidebar and window title)"
-  echo "  --volicon icon.icns"
-  echo "      set volume icon"
-  echo "  --background pic.png"
-  echo "      set folder background image (provide png, gif, jpg)"
-  echo "  --window-pos x y"
-  echo "      set position the folder window"
-  echo "  --window-size width height"
-  echo "      set size of the folder window"
-  echo "  --text-size text_size"
-  echo "      set window text size (10-16)"
-  echo "  --icon-size icon_size"
-  echo "      set window icons size (up to 128)"
-  echo "  --icon file_name x y"
-  echo "      set position of the file's icon"
-  echo "  --hide-extension file_name"
-  echo "      hide the extension of file"
-  echo "  --custom-icon file_name custom_icon_or_sample_file x y"
-  echo "      set position and custom icon"
-  echo "  --app-drop-link x y"
-  echo "      make a drop link to Applications, at location x,y"
-  echo "  --eula eula_file"
-  echo "      attach a license file to the dmg"
-  echo "  --no-internet-enable"
-  echo "      disable automatic mount&copy"
-  echo "  --version         show tool version number"
-  echo "  -h, --help        display this help"
-  exit 0
-}
-
-WINX=10
-WINY=60
-WINW=500
-WINH=350
-ICON_SIZE=128
-TEXT_SIZE=16
-
-echo "ENTER DMG name (e.g. AirtableInstaller(.dmg))"
-read DMG_PATH
+while [ -z "$DMG_PATH" ]; do
+  echo "ENTER DMG Path (REQUIRED e.g. ~/Desktop/AirtableInstaller.dmg)"
+  read DMG_PATH
+done
 if [ ! ${DMG_PATH: -4} == ".dmg" ] || (( ${#DMG_PATH} < 4 )); then
   DMG_PATH+=".dmg"
-  echo "$DMG_PATH"
+  echo "DMG PATH: $DMG_PATH"
 fi
-echo "Enter Volume Name:"
+
+echo "Enter Volume Name: (Airtable)"
 read VOLUME_NAME
+if [ -z "$VOLUME_NAME" ]; then
+  VOLUME_NAME="Airtable"
+fi
 
 echo "Enter Volume Icon File Name or Path (icon.icns)"
 read VOLUME_ICON_FILE
+if [ -z "$VOLUME_ICON_FILE" ]; then
+  VOLUME_ICON_FILE="icon.icns"
+fi
 
 echo "Enter Background Image File Name or Path (background.png)"
 read BACKGROUND_FILE
-if  [ ! -z "$BACKGROUND_FILE" ]; then
-  BACKGROUND_FILE_NAME="$(basename $BACKGROUND_FILE)"
-  BACKGROUND_CLAUSE="set background picture of opts to file \".background:$BACKGROUND_FILE_NAME\""
-  REPOSITION_HIDDEN_FILES_CLAUSE="set position of every item to {theBottomRightX + 100, 100}"
+if [ -z "$BACKGROUND_FILE" ]; then
+  BACKGROUND_FILE="background.png"
 fi
-echo "Enter Icon Size (recommended: 152)"
+BACKGROUND_FILE_NAME="$(basename $BACKGROUND_FILE)"
+BACKGROUND_CLAUSE="set background picture of opts to file \".background:$BACKGROUND_FILE_NAME\""
+REPOSITION_HIDDEN_FILES_CLAUSE="set position of every item to {theBottomRightX + 100, 100}"
+
+echo "Enter Icon Size (152)"
 read ICON_SIZE
 if [ -z $ICON_SIZE ]; then
   ICON_SIZE=152
 fi
-echo "Enter Text Size (recommended: 12)"
+echo "Icon size set to $ICON_SIZE"
+
+echo "Enter Text Size (12)"
 read TEXT_SIZE
-if [ -z  $TEXT_SIZE ]; then
+if [ -z $TEXT_SIZE ]; then
   TEXT_SIZE=12
 fi
-while [[ ${#WIN_POS[@]} < 2 ]]; do
-   echo "Enter Window Position x,y (example: 300,250)"
+echo "Text size set to $TEXT_SIZE"
+
+while [ ${#WIN_POS[@]} -lt 2 ] ; do
+   echo "Enter Window Position x,y (300,250)"
    read WIN_POS_S
+   if [ -z "$WIN_POS_S"]; then 
+    WIN_POS_S="300,250"
+   fi
    IFS=',' read -ra WIN_POS <<< "$WIN_POS_S"
  done 
 WINX=${WIN_POS[0]}
 WINY=${WIN_POS[1]}
 
 while [[ ${#WIN_SIZE[@]} < 2 ]]; do
-   echo "Enter Window Size width,height (example: 650,300)"
+   echo "Enter Window Size width,height (610,310)"
    read WIN_SIZE_S
+   if [ -z "$WIN_SIZE_S" ]; then
+    WIN_SIZE_S="610,310"
+   fi
    IFS=',' read -ra WIN_SIZE <<< "$WIN_SIZE_S"
  done 
 WINW=${WIN_SIZE[0]}
 WINH=${WIN_SIZE[1]}
+echo "WinX:$WINX WinY:$WINY width:$WINW height:$WINH"
 
-echo "Enter Application Path (e.g.: Airtable.app)"
-read APP_PATH
+
+while [ -z "$APP_PATH" ]; do
+  echo "Enter Application Path (REQUIRED e.g.: ../Airtable.app)"
+  read APP_PATH
+done
 APP_NAME="$(basename $APP_PATH)"
-echo "$APP_NAME"
+echo "APP NAME: $APP_NAME"
+
 while [[ ${#APP_POS[@]} < 2 ]]; do
    echo "Enter Application Icon Postion x,y (example: 120,150)"
    read APP_POS_S
+   if [ -z "$APP_POS_S" ]; then
+    APP_POS_S="120,150"
+   fi
    IFS=',' read -ra APP_POS <<< "$APP_POS_S"
  done 
 APPX=${APP_POS[0]}
 APPY=${APP_POS[1]}
 POSITION_CLAUSE="set position of item \"$APP_NAME\" to {$APPX, $APPY}"
+echo "$POSITION_CLAUSE"
 
 while [[ ${#DROP_POS[@]} < 2 ]]; do
-   echo "Enter Applications Folder Icon Postion x,y (example: 600,150)"
+   echo "Enter Applications Folder Icon Postion x,y (example: 490,150)"
    read DROP_POS_S
+   if [ -z "$DROP_POS_S" ]; then
+    DROP_POS_S="490,150"
+   fi
    IFS=',' read -ra DROP_POS <<< "$DROP_POS_S"
  done 
 DROPX=${DROP_POS[0]}
 DROPY=${DROP_POS[1]}
 
 APPLICATION_CLAUSE="set position of item \"Applications\" to {$DROPX, $DROPY}"
+echo "$APPLICATION_CLAUSE"
 
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-# DMG_PATH="$1"
 DMG_DIRNAME="$(dirname "$DMG_PATH")"
 DMG_DIR="$(cd "$DMG_DIRNAME" > /dev/null; pwd)"
 DMG_NAME="$(basename "$DMG_PATH")"
@@ -188,7 +169,7 @@ if ! test -z "$VOLUME_ICON_FILE"; then
   SetFile -a C "$MOUNT_DIR"
 fi
 
-
+# fixes window postion, window size, icon size, text size, and positions
 APPLESCRIPT=$(mktemp -t createdmg)
 cat "$AUX_PATH/template.applescript" | sed -e "s/WINX/$WINX/g" -e "s/WINY/$WINY/g" -e "s/WINW/$WINW/g" -e "s/WINH/$WINH/g" -e "s/BACKGROUND_CLAUSE/$BACKGROUND_CLAUSE/g" -e "s/REPOSITION_HIDDEN_FILES_CLAUSE/$REPOSITION_HIDDEN_FILES_CLAUSE/g" -e "s/ICON_SIZE/$ICON_SIZE/g" -e "s/TEXT_SIZE/$TEXT_SIZE/g" | perl -pe  "s/POSITION_CLAUSE/$POSITION_CLAUSE/g" | perl -pe "s/APPLICATION_CLAUSE/$APPLICATION_CLAUSE/g">"$APPLESCRIPT"
 
@@ -198,6 +179,16 @@ echo "Done running the applescript..."
 sleep 4
 
 rm "$APPLESCRIPT"
+
+# make sure it's not world writeable
+echo "Fixing permissions..."
+chmod -Rf go-w "${MOUNT_DIR}" &> /dev/null || true
+echo "Done fixing permissions."
+
+# opens folder on mount
+echo "Blessing started"
+bless --folder "${MOUNT_DIR}" --openfolder "${MOUNT_DIR}"
+echo "Blessing finished"
 
 
 echo "Disk image done"
